8a4932cc9882346ef4c12e25a5c1e5a6c2ddcf3c
hesterconsoles <94391552+hesterconsoles@users.noreply.github.com>
2023-08-24 13:43:41 +0800

    Add files via upload

---
+++ capybara_website.py
@@ -0,0 +1,51 @@
+import time
+import pyttsx3
+from flask import Flask, render_template
+
+website=Flask(__name__)
+@website.route('/')
+def input_content():
+    #language = input('pls input your wish: ')
+    return render_template('capybara.html')
+
+
+if __name__ == '__main__':
+    website.run(host='0.0.0.0',debug=True)
+
+
+'''
+engine=pyttsx3.init()
+def sayoutloud(content):
+
+    if gender == 'F':
+        voice = engine.getProperty('voices')
+        engine.setProperty('voice', 'com.apple.speech.synthesis.voice.tingting')
+    else:
+        pass
+
+    #engine.say('明天要上班')
+    engine.say(content)
+    engine.runAndWait()
+
+
+
+language=input('pls input your wish: ')
+#gender = input('what kind of voice you want? M/F: ')
+
+
+total=language.count("!")
+while total>=0:
+    if language.count("!")==0:
+        print(f'{language}' + 'capybara~')
+        sayoutloud(f'{language}'+'capybara~')
+
+        
+    else:
+        print(f'{language}' + 'capybara~' * language.count('!'))
+        sayoutloud(f'{language}'+'capybara~'*language.count('!'))
+
+
+    time.sleep(0.1)
+    total-=1
+
+'''
